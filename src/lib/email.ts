421638293c324f94847005e7da37449867798bc7
Abz Mirzaev <abzmirzaev@gmail.com>
2026-05-29 03:53:51 +0500

    feat: email from info@tarjumanedu.com via resend

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -19,7 +19,7 @@ export async function sendEmail({ to, subject, html, text }: EmailOptions) {
       'Content-Type':  'application/json',
     },
     body: JSON.stringify({
-      from:    'TARJUMAN <onboarding@resend.dev>',
+      from:    'TARJUMAN <info@tarjumanedu.com>',
       to:      [to],
       subject,
       html,
