33285e6ec3536f7890344375a4c2fc43c77572e8
Abz Mirzaev <abzmirzaev@gmail.com>
2026-06-12 14:53:44 +0500

    fix: repair corrupted files + add Al Qasimia FAQ page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -228,15 +228,3 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     </html>
   )
 }
-tInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-        />
-      </head>
-      <body>
-        <Toaster position="top-center" richColors />
-        <TelegramButton />
-        {children}
-        <Footer />
-      </body>
-    </html>
-  )
-}
